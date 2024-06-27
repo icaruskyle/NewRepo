3b2c7c392ef9562895745ea769d94eb12803ebe5
Aldrin Kyle <Aldrin Kyle@KYLE-BOOK19>
2024-06-27 20:08:09 +0800

    Add project files.

---
+++ OrderingSystem/product/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace product
+{
+    public class Product
+    {
+       <List>Products 
+            
+            static void AddProduct(string, double)
+        {
+            Product prod = new Product();
+            prod.name = n;
+            prod.name = p;
+            products.Add(prod);
+        }
+        AddProduct (Product prod)
+        {
+            products.Add(prod);
+        }
+        static DisplayMenu (string tite)
+        {
+            foreach () ;
+        }
+    }
+}
